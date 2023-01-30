78e633fae4d7150d891ba581752819e7bd229ec3
DavidMoralesLuis <dmoralesluis@danielcastelao.org>
2023-01-30 13:12:49 +0100

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float force = 150f;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 vector = new Vector3(h, 0.5f, v);
+
+        rb.AddForce(vector * force * Time.deltaTime);
+        
+        
+    }
+}
